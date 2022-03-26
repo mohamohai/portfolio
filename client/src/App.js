08dffeaa9feecef27787be095ec20cc44ab48086
mohamohai <ahwjs1022@gmail.com>
2022-03-26 17:53:58 +0900

    route v6 browser > routes > route

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,10 @@ import './mainCss/gnbContainer.css';
 import React, {useState}         from 'react';
 
 import GNB           from './mainContents/GNB';
-import UserAdd       from './Components/UserAdd.js';
-import SelectUser    from './Components/SelectUser';
-import DeleteUser    from './Components/DeleteUser';
-import UpdateUser    from './Components/UpdateUser';
-import SchedulePop   from './Components/SchedulePop';
-import SignUp        from './Components/SignUp';
 
-import DivClear      from './mainContents/divClear';
-import axios         from 'axios';
-
-import PopupDom from './PopupDom';
-import { Calendar } from 'react-date-range';
-import "react-date-range/dist/styles.css"; // main style file 
-import 'react-date-range/dist/theme/default.css'; // theme css file  
 import Home from './inc/home.js';
 import Test from './inc/test.js';
-import TestTwo from './inc/testTwo.js';
+
 
 import { BrowserRouter, Link } from 'react-router-dom';
 
@@ -37,15 +24,20 @@ class App extends Component{
   render(){
   
     return(
+<>  
+      <nav>
+      <Link to ="/">Home</Link>
+      <Link to ="/test">sss</Link>
+
+      </nav>
 <BrowserRouter>
-      <Router>
-        <Routes>
-          <Route path="/"        Component={Home}></Route>
-          <Route path="/test"    Component={Test} ></Route>
-        </Routes>
-      </Router>
-          
-      </BrowserRouter>
+
+      <Routes>
+        <Route path ="*" element={<Home/>}/>
+        <Route path ="/test" element={<Test/>}/>
+      </Routes>
+</BrowserRouter>
+</>
 
        
  
