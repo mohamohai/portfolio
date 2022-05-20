4275a9ca85130261dafb52b9e76509cfcd7db68c
mohamohai <ahwjs1022@gmail.com>
2022-05-20 14:31:18 +0900

    whywhy

--- client/src/mainContents/PracticeTwo.js
+++ client/src/mainContents/PracticeTwo.js
@@ -3,6 +3,8 @@ import { Component } from "react";
 import React, { useState } from 'react';
 import './PracticeTwo.css';
 import axios from "axios";
+import { PresignedPost } from "aws-sdk/clients/s3";
+
   
 class PracticeTwo extends Component{
     
@@ -20,7 +22,7 @@ class PracticeTwo extends Component{
     }
     
     _getData = async () => {
-    const userId = this.state.uid;
+        const userId = this.state.uid;
         const res = await axios.get('/get/ScheduleId',
         {
             params: {userId: userId
@@ -34,23 +36,51 @@ class PracticeTwo extends Component{
     }
     this.setState({ list : res.data });
     }
-
+   
     render(){
-        const mapCnt =[];
-        for(let forCnt = 0 ; forCnt<= 5 ; forCnt++){
-            mapCnt.push(forCnt);
+        const mapCnt  = [0,1,2,3,4,5];
+        const mapCnt2 = [0,1,2,3,4,5,6];
+        const mapCnt3 = [0,1,2,3,4,5]
+        let mapPlus  = 0;
+        let mapPlus2 = 0;
+        // for(let forCnt = 0 ; forCnt<= 5 ; forCnt++){
+        //     mapCnt.push(forCnt);
+        // }
+        function mapfor(nowYear, nowMonth, nowDay, nowWeekNum){
+            let arrayTable = [];
+            for(let i=0; i<=5;i++){
+                for(let j=0; j<=6;j++){
+                    if(j==0){
+                        arrayTable.push(<div className="sunDay sunTitle"></div>)
+                    }else if(j==1){
+                        arrayTable.push(<div className="monDay">bb</div>)
+                    }else if(j==2){
+                        arrayTable.push(<div className="tuesDay">cc</div>)
+                    }else if(j==3){
+                        arrayTable.push(<div className="wednesDay">dd</div>)
+                    }else if(j==4){
+                        arrayTable.push(<div className="thursDay">ee</div>)
+                    }else if(j==5){
+                        arrayTable.push(<div className="friDay">ff</div>)
+                    }else if(j==6){
+                        arrayTable.push(<div className="saturDay">gg</div>)
+                    }
+                    
+                }
+            }return arrayTable;
         }
-       
+        
         const { list } = this.state;                
         const numbers = [1, 3, 5]; 
         const numbers2 = [2, 4, 6];
             const aloop = numbers.map((number, idx) => {
              return number});
 
-            mapCnt.map((cnt,idx) => {
-                return console.log(cnt)
-            });    
-     
+            
+        
+      
+        
+
         let nowDate  = new Date();//현재 날짜와 시간
         let nowYear  = nowDate.getFullYear();
         let nowMonth = nowDate.getMonth()+1;
@@ -63,7 +93,19 @@ class PracticeTwo extends Component{
         let test = new Date(testin).getDay();
         
         console.log(nowYear + '년 ' +nowMonth +'월 ' +nowDay +'일 ' + nowWeekArray[nowWeekNum] + '요일 '+nowWeekNum);
+        let arrSchedule = [];
+        let scheduleList  =  list.map((i,idx) => {
+                arrSchedule.push(i);
+                console.log(i.account);
                 
+        })
+
+        function callarr(){
+            for( let i = 0; i<arrSchedule.length; i++ )
+                if(arrSchedule[i].time==20220516)
+                    console.log(arrSchedule[2].id);
+            }
+          
         return(
         <div className = "">   
         <div className="selectMon">
@@ -92,7 +134,12 @@ class PracticeTwo extends Component{
             </ul>
         </div>
         <div>
-        {mapCnt.map((cnt,idx) => {
+            {
+            
+            callarr()
+            }
+       
+        {/* { mapCnt.map((cnt,idx) => {
             if(cnt)
                 return (<div className="weekLine">
                             <div className="sunDay sunDay1">
@@ -106,9 +153,9 @@ class PracticeTwo extends Component{
                         )
                         })  
                               
-            } 
+            }  */}
     
-            {mapCnt.map((cnt,idx) => { //42루프, 6루프7개  div안에 네임 적기
+            {/* {mapCnt.map((cnt,idx) => { //42루프, 6루프7개  div안에 네임 적기
             // 맵을 2줄로 바꿔서 인자값 두개, 하나를 요일값 하나를 줄로 설정해서 classname 설정하기
                 return (<div className="weekLine">
                             <div className="sunDay sunDay1">
@@ -157,10 +204,8 @@ class PracticeTwo extends Component{
                         )
                         })  
                               
-            } 
-            </div> 
-            
-            
+            }   */}
+            </div>  
         </div>
         );
     }    
