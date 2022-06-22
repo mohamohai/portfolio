ab79445443ab85a6ba74da7bf73e2af4fe0f749f
mohamohai <ahwjs1022@gmail.com>
2022-06-22 16:15:33 +0900

    modal data

--- client/src/Components/Schedule/ScheduleExport.js
+++ client/src/Components/Schedule/ScheduleExport.js
@@ -1,6 +1,7 @@
 export { default as ScheduleCalendarMonth } from "./ScheduleCalendarMonth";
 export { default as ScheduleSelect } from "./ScheduleSelect";
 export { default as ScheduleDelete } from "./ScheduleDelete"; //필수
+
 export { default as ScheduleAdd } from "./ScheduleAdd"; // 필요
 export { default as ScheduleView } from "./ScheduleView";
 export { default as ScheduleId } from "./ScheduleCal";
@@ -8,3 +9,4 @@ export { default as ScheduleMain } from "./ScheduleMain"; //필요
 export { default as ScheduleViewerModal } from "./ScheduleViewerModal.js"; //필수
 export { default as testView } from "./testView.js";
 export { default as testViewTwo } from "./testViewTwo.js";
+export { default as ScheduleDeleteModal } from "./ScheduleDeleteModal.js";
